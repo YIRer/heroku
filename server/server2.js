10032dcfb919a008c8687dd6e29c3a280feb57b8
Evan <evan@culturehero.net>
2018-06-19 14:01:35 +0900

    compress

--- server/server2.js
+++ server/server2.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import Loadable from 'react-loadable';
 import render from './render2';
- 
+
 const path = require("path");
+const compression = require("compression");
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const router = express.Router();
+
+app.use(compression({
+  level: 2,               // set compression level from 1 to 9 (6 by default)
+  filter: shouldCompress, // set predicate to determine whether to compress
+}));
 
 app.get('*.*', express.static(path.join(__dirname)));
 
